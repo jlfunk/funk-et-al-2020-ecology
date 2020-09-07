76689558506c928d61ce9caf988ee6d282b4685c
Mike Macpherson <mmacpherson@users.noreply.github.com>
2020-09-07 08:26:39 -0700

    initial fits

--- multilevel.R
+++ multilevel.R
@@ -2,50 +2,60 @@ library(tidyverse)
 library(brms)
 library(bayesplot)
 
-df <- read_csv("~/Dropbox/Macphunk/jen-drought-and-fitness/REC_raw_all_nooutlier2.csv") %>%
+df <- read_csv("/storage/Dropbox/Macphunk/jen-drought-and-fitness/REC_raw_all_nooutlier2.csv") %>%
   select(Species, Treatment, photo, fitness) %>%
   mutate(
-    Treatment=factor(Treatment, levels = c("L", "M", "H"), ordered = T),
-    photo=(photo - mean(photo, na.rm=T)) / sd(photo, na.rm=T),
-    ) %>%
+    Treatment = factor(Treatment, levels = c("L", "M", "H"), ordered = T),
+    ## -- normalize photo globally
+    photo = (photo - mean(photo, na.rm = T)) / sd(photo, na.rm = T),
+  ) %>%
+  ## -- normalize fitness within each species
   group_by(Species) %>%
   mutate(
-    fitness=(fitness - mean(fitness, na.rm=T)) / sd(fitness, na.rm=T),
-    )
+    fitness = (fitness - mean(fitness, na.rm = T)) / sd(fitness, na.rm = T),
+  )
 
-df %>% summary
+df %>% summary()
 
-(ggplot(df, aes(x=photo, y=fitness))
-  + facet_grid(Species ~ Treatment)
-  + geom_point()
-  + geom_smooth(method = lm)
+# -- photo vs relative fitness
+(ggplot(df, aes(x = photo, y = fitness))
++
+  facet_grid(Species ~ Treatment)
+  +
+  geom_point()
+  +
+  geom_smooth(method = lm)
 )
 
 
-## does photo look normalish?
-(ggplot(df, aes(sample=photo))
-  + facet_grid(Species ~ Treatment)
-  + geom_qq()
-  + geom_qq_line(color="blue")
-)
+## ## does photo look normalish?
+## (ggplot(df, aes(sample=photo))
+##   + facet_grid(Species ~ Treatment)
+##   + geom_qq()
+##   + geom_qq_line(color="blue")
+## )
 
-## and fitness?
-(ggplot(df, aes(sample=fitness))
-  + facet_grid(Species ~ Treatment)
-  + geom_qq()
-  + geom_qq_line(color="blue")
-)
+## ## and fitness?
+## (ggplot(df, aes(sample=fitness))
+##   + facet_grid(Species ~ Treatment)
+##   + geom_qq()
+##   + geom_qq_line(color="blue")
+## )
 
 
 b1.0 <-
-  brm(data = df, family = gaussian,
-      fitness ~ 1 + photo + (1 | Species) + (1 | Treatment),
-      prior = c(prior(normal(0, 10), class = Intercept),
-                prior(normal(0, 10), class = b),
-                prior(cauchy(0, 1), class = sd)),
-      iter = 6000, warmup = 2000, chains = 4, cores = 1,
-      control = list(adapt_delta = 0.99),
-      seed = 99)
+  brm(
+    data = df, family = gaussian,
+    fitness ~ 1 + photo + (1 + photo | Species) + (1 + photo | Treatment),
+    prior = c(
+      prior(normal(0, 10), class = Intercept),
+      prior(normal(0, 10), class = b),
+      prior(cauchy(0, 1), class = sd)
+    ),
+    iter = 6000, warmup = 2000, chains = 4, cores = 4,
+    control = list(adapt_delta = 0.99),
+    seed = 99
+  )
 
 post <- posterior_samples(b1.0, add_chain = T)
 
@@ -56,8 +66,11 @@ post %>%
   theme(legend.position = c(.75, .06))
 
 neff_ratio(b1.0) %>%
-  mcmc_neff()
+  mcmc_neff() +
+  theme_fivethirtyeight()
 
-
+pdf("photo-multilevel.pdf", width = 8, height = 10)
 mcmc_plot(b1.0, pars = c("^r_", "^b_", "^sd_")) +
-  theme(axis.text.y = element_text(hjust = 0))
+  theme(axis.text.y = element_text(hjust = 0)) +
+  theme_fivethirtyeight()
+dev.off()
